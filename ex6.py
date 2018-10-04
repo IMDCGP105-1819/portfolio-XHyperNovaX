51bbe31fa9e12714a53680757178f195cae95b88
XHyperNovaX <anthonyguyskipwith@gmail.com>
2018-10-04 11:54:13 +0100

    user input work sheet problem

---
+++ ex6.py
@@ -0,0 +1,19 @@
+name = input("What is your name?: \n> ")
+age = input("How old are you?: \n> ")
+height = input("How tall are you in inches?: \n> ")
+weight = input("How heavy are you in kilograms?: \n> ")
+eyeColour = input("What colour are your eyes?: \n> ")
+hairColour = input("What colour is your hair?: \n> ")
+
+if (int(age)>=12 and int(age)<=19):
+  print("You're a teenager")
+if (int(age)>=100):
+  print("Merry birthday")
+if (int(height)>5):
+  print("You're about average height")
+if (int(height)<100):
+  print("Don't worry, it's normal to be shorter than 100 inches tall")
+if (int(weight)>100):
+  print(":))))))))))))")
+if (int(weight)<30):
+  print("((((((((((((:")
