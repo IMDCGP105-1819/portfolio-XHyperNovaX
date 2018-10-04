645f4eacabd46f4bebc53df4ba2140f169ad2a9c
XHyperNovaX <anthonyguyskipwith@gmail.com>
2018-10-04 11:35:36 +0100

    beer bottles on a wall

---
+++ ex5.py
@@ -0,0 +1,5 @@
+for x in range(99,0,-1):
+    if x > 1:
+        print(str(x)+" bottles of beer on the wall, "+str(x)+" bottles of beer. Take one down, pass it around, "+str(x-1)+" bottles of beer on the wall")
+    if x == 1:
+        print(str(x)+" bottles of beer on the wall, "+str(x)+" bottles of beer. Take one down, pass it around, no more bottles of beer on the wall :c")
